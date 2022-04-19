83fe02d208fd2ebe848b0bc788161a244b9fe013
A S M Saeduzzaman <saeduzzaman@mislbd.com>
2022-04-19 20:12:16 +0600

    Users Module Created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) }
+  { path: 'login', component: LoginComponent},
+  { path: 'sign-up', component: RegistrationComponent},
+  { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
+  { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) }
 ];
 
 @NgModule({
