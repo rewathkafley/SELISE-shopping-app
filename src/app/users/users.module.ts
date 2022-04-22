02f1ad3faeeba63e275b93b1d8ea5508d6ce8681
A S M Saeduzzaman <saeduzzaman@mislbd.com>
2022-04-23 00:19:31 +0600

    User Details Component Created

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 
 @NgModule({
   declarations: [
-    UsersComponent
+    UsersComponent,
+    UserDetailsComponent,
+    UserFormComponent
   ],
   imports: [
     CommonModule,
