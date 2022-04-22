ae4e3193cc0db57b38d55c2f7f4d2855fa78b1fc
A S M Saeduzzaman <saeduzzaman@mislbd.com>
2022-04-23 00:27:22 +0600

    Routes Updated

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductFormComponent } from './product-form/product-form.component';
 import { ProductsComponent } from './products.component';
 
 const routes: Routes = [
   { path: '', component: ProductsComponent },
-  { path: ':id', component: ProductDetailsComponent}
+  { path: 'create', component: ProductFormComponent },
+  { path: ':id', component: ProductDetailsComponent },
+  { path: ':id/edit', component: ProductFormComponent }
 ];
 
 @NgModule({
--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { UserFormComponent } from './user-form/user-form.component';
 import { UsersComponent } from './users.component';
 
-const routes: Routes = [{ path: '', component: UsersComponent }];
+const routes: Routes = [
+  { path: '', component: UsersComponent },
+  { path: 'create', component: UserFormComponent },
+  { path: ':id', component: UserDetailsComponent },
+  { path: ':id/edit', component: UserFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
