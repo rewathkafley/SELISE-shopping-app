40ba60ff82ae3c247586e61c20adcfcea1575489
A S M Saeduzzaman <saeduzzaman@mislbd.com>
2022-04-22 14:30:08 +0600

    User Model Created

---
+++ src/app/models/User.model.ts
@@ -0,0 +1,9 @@
+export class User {
+    id: number | undefined;
+    userName: string | undefined;
+    firstName: string | undefined;
+    lastName: string | undefined;
+    password: string | undefined;
+    role: string | undefined;
+    token?: string;
+}
