75ce2cf867f525ea4583e0c24429842864fec4db
A S M Saeduzzaman <saeduzzaman@mislbd.com>
2022-04-24 02:13:58 +0600

    Product Details Component finalized

--- src/app/products/product-details/product-details.component.ts
+++ src/app/products/product-details/product-details.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../../models/Product.model';
+import { Role } from '../../models/Role.model';
+import { AuthService } from '../../services/auth.service';
+import { ProductService } from '../../services/product.service';
+import { BaseComponent } from '../../shared/base-component/base.component';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.scss']
 })
-export class ProductDetailsComponent implements OnInit {
+export class ProductDetailsComponent extends BaseComponent implements OnInit {
+  product: Product = new Product();
+  role: Role = Role.USER;
+  ROLE = Role;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+    private authService: AuthService
+  ) {
+    super();
   }
 
+  ngOnInit(): void {
+    const routeParamMap = this.route.snapshot.paramMap;
+    const productId = Number(routeParamMap.get('productId'));
+    if(productId) {
+      this.subscribers.getProductSub = this.productService.getProduct(productId).subscribe(product => {
+        this.product = product || new Product();
+      })
+    }
+  }
+
+  addToCart() {
+  }
 }
