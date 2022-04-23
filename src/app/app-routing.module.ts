059c13de8c2dec797a0514788788c92d7c136b3c
A S M Saeduzzaman <saeduzzaman@mislbd.com>
2022-04-24 04:04:36 +0600

    App Routing Module Finalized

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './account/login/login.component';
-import { RegistrationComponent } from './account/registration/registration.component';
 import { AccessGuard } from './guards/access.guard';
 import { Role } from './models/Role.model';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
-  { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegistrationComponent},
-  { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
+  { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule), canActivate: [AccessGuard] },
   { 
     path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule),
     canActivate: [AccessGuard],
