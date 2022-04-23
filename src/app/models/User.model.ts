8bd2d252b50f32d1eb3cc23107b3f1393b42fd06
A S M Saeduzzaman <saeduzzaman@mislbd.com>
2022-04-24 02:17:36 +0600

    User Model Updated

--- src/app/models/User.model.ts
+++ src/app/models/User.model.ts
@@ -1,9 +1,12 @@
+import { Role } from "./Role.model";
+
 export class User {
-    id: number | undefined;
-    userName: string | undefined;
-    firstName: string | undefined;
-    lastName: string | undefined;
-    password: string | undefined;
-    role: string | undefined;
+    id: number = 0;
+    userName: string = '';
+    firstName: string = '';
+    lastName: string = '';
+    password: string = '';
+    role: Role = Role.USER;
     token?: string;
+    isLoggedIn: boolean = false;
 }
