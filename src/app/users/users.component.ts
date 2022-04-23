529c28700761c28a47e42c3cb6f3db1370d966cc
A S M Saeduzzaman <saeduzzaman@mislbd.com>
2022-04-24 02:10:26 +0600

    User Component finalized

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Role } from '../models/Role.model';
+import { User } from '../models/User.model';
+import { UserService } from '../services/user.service';
+import { BaseComponent } from '../shared/base-component/base.component';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent extends BaseComponent implements OnInit {
+  users: User[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private userService: UserService
+  ) {
+    super();
   }
 
+  ngOnInit(): void {
+    this.getAllUsers();
+  }
+
+  getAllUsers() {
+    this.subscribers.getAllUsersSub = this.userService.getUsers().subscribe(users => {
+      this.users = users || [];
+    });
+  }
+
+  removeUser(userId: number) {
+    if(!userId) { return; }
+    this.subscribers.removeUserSub = this.userService.deleteUser(userId).subscribe(value => {
+      if(!value) { return; }
+      this.getAllUsers();
+      alert("User removed successfully!");
+    })
+  }
 }
