5b3f341bd8e7d2360ba7e061e8e9e837540b8939
A S M Saeduzzaman <saeduzzaman@mislbd.com>
2022-04-24 05:29:29 +0600

    App Component Cleaned

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { CartService } from './services/cart.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  title = 'Shopping-app';
-
-  constructor(
-    private cartService: CartService
-  ) { }
-  
-  ngOnInit(): void {
-    // not required
-    this.cartService.clearCart();
-  }
-
-  ngOnDestroy(): void {
-  }
+export class AppComponent {
+  title = 'shopping-app';
 }
