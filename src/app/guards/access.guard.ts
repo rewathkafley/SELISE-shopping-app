f10fe8da4921fc00239ec182987d76124ed5a908
A S M Saeduzzaman <saeduzzaman@mislbd.com>
2022-04-24 02:18:21 +0600

    Access Guard Finalized

--- src/app/guards/access.guard.ts
+++ src/app/guards/access.guard.ts
@@ -1,19 +1,40 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccessGuard implements CanActivate, CanActivateChild {
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.validateUserAccess(route, state);
   }
+
   canActivateChild(
     childRoute: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return this.canActivate(childRoute, state);
+  }
+
+  validateUserAccess(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const user = this.authService.getUser();
+    if(!user.isLoggedIn) {
+      this.router.navigate(['/login'], { queryParams: { redirectTo: state.url } } );
+      return false;
+    }
+    if(route.data && route.data['roles'] && (!user.role || route.data['roles'].indexOf(user.role) === -1)) {
+      // In such a case, user can be logged out & navigate to home
+      this.router.navigate(['/']);
+      return false;
+    }
     return true;
   }
 }
