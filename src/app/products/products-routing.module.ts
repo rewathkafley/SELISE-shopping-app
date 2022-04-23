2cf2e296e90a9f84e0ed33458f450817088ef085
A S M Saeduzzaman <saeduzzaman@mislbd.com>
2022-04-24 04:04:08 +0600

    Product Routing Module Finalized

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccessGuard } from '../guards/access.guard';
+import { Role } from '../models/Role.model';
+import { CheckOutComponent } from './check-out/check-out.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductFormComponent } from './product-form/product-form.component';
 import { ProductsComponent } from './products.component';
 
 const routes: Routes = [
   { path: '', component: ProductsComponent },
-  { path: 'create', component: ProductFormComponent },
-  { path: ':id', component: ProductDetailsComponent },
-  { path: ':id/edit', component: ProductFormComponent }
+  { path: 'create', component: ProductFormComponent, canActivate: [AccessGuard], data: { roles: Role.ADMIN } },
+  { path: ':productId', component: ProductDetailsComponent },
+  { path: ':productId/edit', component: ProductFormComponent, canActivate: [AccessGuard], data: { roles: Role.ADMIN } },
+  { path: 'cart/checkout', component: CheckOutComponent }
 ];
 
 @NgModule({
