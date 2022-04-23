9d22c4c8a56a88b5f790246ce4fca94fdf073c98
A S M Saeduzzaman <saeduzzaman@mislbd.com>
2022-04-24 02:11:48 +0600

    Shared Module Updated

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,29 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { BaseComponent } from './base-component/base.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserRegistrationFormComponent,
+    BaseComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BaseComponent,
+    UserRegistrationFormComponent
   ]
 })
 export class SharedModule { }
