fc7e9a366b85179b057ce342bff2e9fb590e75f0
A S M Saeduzzaman <saeduzzaman@mislbd.com>
2022-04-24 02:07:34 +0600

    User Form Component Finalized

--- src/app/users/user-form/user-form.component.ts
+++ src/app/users/user-form/user-form.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Role } from '../../models/Role.model';
+import { User } from '../../models/User.model';
+import { UserService } from '../../services/user.service';
+import { BaseComponent } from '../../shared/base-component/base.component';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.scss']
 })
-export class UserFormComponent implements OnInit {
+export class UserFormComponent extends BaseComponent implements OnInit {
+  userId!: number;
+  role: Role = Role.ADMIN;
+  title: string = "Create User";
+  @Input() showSubmitLoader: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private userService: UserService
+  ) {
+    super();
   }
 
+  ngOnInit(): void {
+    this.subscribers.routeSub = this.route.params.subscribe(params => {
+      if(params['userId']) {
+        this.userId = params['userId'];
+        this.title = "Update User";
+      }
+    });
+  }
+  
+  onUserFormSubmit(formData: User) {
+    if(!formData) { return; }
+    if(formData.id) {
+      this.updateUser(formData);
+    } else {
+      this.createUser(formData);
+    }
+  }
+
+  createUser(user: User) {
+    if(!user) { return; }
+    this.subscribers.createUserSub = this.userService.addUser(user).subscribe(userCreated => {
+      if(!userCreated || !userCreated.id) { return; }
+      alert("User created successfully!");
+      this.router.navigate(['/users', userCreated.id]);
+    });
+  }
+
+  updateUser(user: User) {
+    if(!user) { return; }
+    this.subscribers.updateUserSub = this.userService.updateUser(user).subscribe(userUpdated => {
+      if(!userUpdated || !userUpdated.id) { return; }
+      alert("User updated successfully!");
+      this.router.navigate(['/users', userUpdated.id]);
+    });
+  }
+
+  back() {
+    this.router.navigate(['../'], { relativeTo: this.route });
+  }
 }
