e9d15843dea70c231a84090a03515a1031257a25
A S M Saeduzzaman <saeduzzaman@mislbd.com>
2022-04-24 02:13:01 +0600

    Auth Service finalized

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,39 +1,56 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { Role } from '../models/Role.model';
 import { User } from '../models/User.model';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  // isLoggedIn: boolean = false;
-  // userRole: string = '';
   user: User = new User();
+  loginNotifier = new BehaviorSubject(new User());
+  loginObservable$ = this.loginNotifier.asObservable();
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private userService: UserService
+  ) { }
 
-  login(userName: string, password: string, role: Role) {
-    const user = new User();
+  checkLogin(userName: string, password: string): Observable<any> {
     if(!userName || !password) {
-      this.setUser(user);
-      return;
+      this.setUser(new User());
+      this.notifyLoggedIn(new User());
+      return of(false);
     }
-    user.userName = userName;
-    user.password = password;
-    user.role = role;
+    return this.userService.getUserByNameAndPassword(userName, password);
+  }
+
+  login(user: User) {
+    user = user || new User();
     this.setUser(user);
+    this.notifyLoggedIn(user);
   }
 
   getUser() {
-    return this.user;
+    return this.user || new User();
   }
 
   setUser(user: User) {
     this.user = user;
+    // localStorage not being used, intentionally avoided using local/session storage, as these can be modified easily & cannot be checked against any backend auth
+    // as we are not using any backend here
     localStorage.setItem('user', JSON.stringify(this.user));
   }
 
   logOut() {
     this.setUser(new User());
+    this.router.navigate(['home']);
+    this.notifyLoggedIn(new User());
+  }
+
+  notifyLoggedIn(user: User) {
+    this.loginNotifier.next(user);
   }
 }
